34a8223e1108e58c84593c03aed3bc9feea3b742
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-05-16 18:40:07 +0200

    The profile view controller was made in storyboard

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Антон Кашников on 16.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var photoImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+}
