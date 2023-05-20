c53d4f0c8ed1cfe173080b4f069b2adbeb3ba19f
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-05-20 21:25:53 +0200

    Added bad zooming to Scroll View

--- SingleImageView/SingleImageViewController.swift
+++ SingleImageView/SingleImageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     // MARK: - Public Properties
     var image: UIImage! {
@@ -18,7 +19,26 @@ final class SingleImageViewController: UIViewController {
                 return
             }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        view.layoutIfNeeded()
+        
+        let visibleRectSize = scrollView.bounds.size
+        let hScale = visibleRectSize.width / image.size.width
+        let vScale = visibleRectSize.height / image.size.height
+        let scale = min(scrollView.maximumZoomScale, max(scrollView.minimumZoomScale, max(hScale, vScale)))
+        
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
     // MARK: - UIViewController
@@ -26,10 +46,32 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         
         imageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
     // MARK: - IBAction
     @IBAction private func didTapBackButton() {
         dismiss(animated: true)
+    }
+    
+    @IBAction private func didTapShareButton() {
+        guard let image else {
+            print("No image found")
+            return
+        }
+        
+        let shareVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(shareVC, animated: true)
+    }
+}
+
+    // MARK: - UIScrollViewDelegate
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
