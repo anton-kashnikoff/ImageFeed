285e341e9f4aae4f9903aebe9736b661619cb2a2
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-05-27 14:08:42 +0200

    Added constants for API keys and URI

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Антон Кашников on 27.05.2023.
+//
+
+import Foundation
+
+let accessKey = "LTfzHd9v1j8p7Tq7xSD9QuI6eb-HVaezC3Or_OCOJLc"
+let secretKey = "crW2j2ot4HrvhyX-9VKhmKqoWxpQeYRZi6666_DxCDM"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
