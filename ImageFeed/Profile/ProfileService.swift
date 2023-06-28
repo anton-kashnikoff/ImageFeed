4ce889ec8e1dd105fc7a950e8b49c7bbf7e7c5f0
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-06-28 16:16:51 +0200

    Provided a default value for last name string

--- ImageFeed/Profile/ProfileService.swift
+++ ImageFeed/Profile/ProfileService.swift
@@ -29,7 +29,7 @@ struct Profile {
 
     init(profileResult: ProfileResult) {
         self.username = profileResult.username
-        self.name = "\(profileResult.firstName) \(profileResult.lastName)"
+        self.name = "\(profileResult.firstName) \(profileResult.lastName ?? "")"
         self.loginName = "@\(profileResult.username)"
         self.bio = profileResult.bio ?? ""
     }
